e235d05d9a659bf497b1c35ac8003e9e79644b5f
Ben Agnew <48106124+bgnw@users.noreply.github.com>
2021-09-28 12:57:38 +0100

    Update DB schema for embed support (future feature)

--- db/mysql-scheduled-reminders-create.sql
+++ db/mysql-scheduled-reminders-create.sql
@@ -1,10 +1,12 @@
 create table reminders_bot__scheduled_reminders
 (
-    reminderID        INT            AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    timestamp         DATETIME(0)    NOT NULL,
-    content           VARCHAR(5000)  NOT NULL,
-    userID            VARCHAR(66)    NOT NULL,
-    guildID           VARCHAR(66)    NOT NULL,
-    channelID         VARCHAR(66)    NOT NULL,
+    reminderID        INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    timestamp         DATETIME(0)        NOT NULL,
+    title             VARCHAR(256),
+    body              VARCHAR(60000)     NOT NULL,
+    embed             BOOLEAN            NOT NULL,
+    userID            VARCHAR(66)        NOT NULL,
+    guildID           VARCHAR(66)        NOT NULL,
+    channelID         VARCHAR(66)        NOT NULL,
     mentionableRoleID VARCHAR(66)
 );
